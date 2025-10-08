f74b712a55961e4e6f0979414a0774a47343c2b3
Steve Loveless <steve.loveless@gmail.com>
2025-10-08 13:17:50 -0700

    Refactor and find males who survived

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use polars::prelude::*;
 
 fn main() -> anyhow::Result<()> {
-    do_test_data()?;
     do_train_data()?;
+    do_test_data()?;
 
     Ok(())
 }
@@ -12,33 +12,32 @@ fn do_train_data() -> anyhow::Result<()> {
         .with_has_header(true)
         .finish()?;
 
-    // let first_train = train_data.first().collect()?;
-    // dbg!(first_train);
+    let first_train = train_data.clone().first().collect()?;
+    println!("First row of training data: {}", first_train);
 
+    percentage_of_sex_who_survived(train_data.clone(), "female")?;
+    percentage_of_sex_who_survived(train_data.clone(), "male")?;
+
+    Ok(())
+}
+
+fn percentage_of_sex_who_survived(train_data: LazyFrame, sex: &str) -> anyhow::Result<()> {
     let women = train_data
-        .filter(col("Sex").eq(lit("female")))
-        // .select([col("Survived")])
+        .filter(col("Sex").eq(lit(sex)))
         .select([
             col("Survived").sum().alias("survived_sum"),
             col("Survived").count().alias("total_count"),
         ])
         .collect()?;
 
-    // let survived_sum: f64 = women.column("Survived")?.sum_reduce()?.into();
-    //
-    // let total_count = women.height() as f64;
-    // let rate_women = survived_sum / total_count;
-
-    // let women = train_data
-    //     .filter(col("Sex").eq("female"))
-    //     .select([col("Survived")]);
-    //
-    // let rate_women = women.sum::<Option<u32>>().collect()? / women.count().collect()?;
-    // dbg!(women);
-
     let rate = women.column("survived_sum")?.get(0)?.try_extract::<f64>()?
         / women.column("total_count")?.get(0)?.try_extract::<f64>()?;
-    dbg!(rate);
+
+    println!(
+        "Percentage of {sex}s who survived: {:.2}% ({rate})",
+        rate * 100.0
+    );
+
     Ok(())
 }
 
@@ -48,7 +47,7 @@ fn do_test_data() -> anyhow::Result<()> {
         .finish()?;
 
     let first_test = test_data.first().collect()?;
-    dbg!(first_test);
+    println!("First row of test data: {}", first_test);
 
     Ok(())
 }
